17850d6adb00f6bc17af6ce95149a9c0a274a28b
alumno155ecb16 <alexandramartinez849@gmail.com>
2014-12-08 15:02:53 -0600

    4subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,21 +1,25 @@
 //JavaScript
 $(document).ready(function(e){	
 $('#principal').height($('#page1').height());
-document.addEventListener("deviceready",function(){
+	document.addEventListener("deviceready",function(){
+		
+		audio=window.plugins.LowLatencyAudio;
+		
+		audio.preloadFX('do','audio/DO.mp3', function(){},function(e){alert('Error '+e);});
+		audio.preloadFX('re','audio/RE.mp3', function(){},function(e){alert('Error '+e);});
+		audio.preloadFX('mi','audio/MI.mp3', function(){},function(e){alert('Error '+e);});
+		audio.preloadFX('fa','audio/FA.mp3', function(){},function(e){alert('Error '+e);});
+		audio.preloadFX('sol','audio/SOL.mp3', function(){},function(e){alert('Error '+e);});
+		audio.preloadFX('la','audio/LA.mp3', function(){},function(e){alert('Error '+e);});
+		audio.preloadFX('si','audio/SI.mp3', function(){},function(e){alert('Error '+e);});
+		
+$('.nota').bind('touchstart', function(){
+	$(this).addClass('tocada');
+	audio.play($(this).attr('id'));
 	
-	audio=window.plugins.LowLatencyAudio;
-	
-	audio.preload('do','audio/DO.mp3',function(){},function(e){alert('Error' +e);});
-	audio.preload('re','audio/RE.mp3',function(){},function(e){alert('Error' +e);});
-	audio.preload('mi','audio/MI.mp3',function(){},function(e){alert('Error' +e);});
-	audio.preload('fa','audio/FA.mp3',function(){},function(e){alert('Error' +e);});
-	audio.preload('sol','audio/SOL.mp3',function(){},function(e){alert('Error' +e);});
-	audio.preload('la','audio/LA.mp3',function(){},function(e){alert('Error' +e);});
-	audio.preload('si','audio/SI.mp3',function(){},function(e){alert('Error' +e);});
-	
-	$('.nota').bind('touchstart',function(){
-		$(this).addClass('tocada');
+	}).bind('touchend',function(){
+		$(this).removeClass('tocada');
 	});
 	
-},false);//deviceready
+	},false); //deviceready
 });//ready
